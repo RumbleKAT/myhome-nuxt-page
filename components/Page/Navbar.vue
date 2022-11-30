3388e81d4531726fd712b33ade7e66b7d21fd63f
RumbleKAT <reki318@naver.com>
2022-11-30 23:02:50 +0900

    (fix)mobile navbar

--- components/Page/Navbar.vue
+++ components/Page/Navbar.vue
@@ -84,66 +84,66 @@ const menus = computed((): IMenuItem[] => [
         <!--        </div>-->
       </div>
     </template>
-    <!--    <template #options="{ toggleOptions }">-->
-    <!--      <ActionSheet @onClose="toggleOptions(false)">-->
-    <!--        <ActionSheetBody>-->
-    <!--          <ActionSheetHeader text="Menu" />-->
-    <!--          <nav class="leading-6 font-semibold text-gray-600 dark:text-gray-300">-->
-    <!--            <ul class="flex flex-col">-->
-    <!--              <li-->
-    <!--                v-for="(item, i) in menus"-->
-    <!--                :key="i"-->
-    <!--                class="flex w-full"-->
-    <!--                :class="{-->
-    <!--                  'pb-2 mb-2 border-b border-gray-900/10 dark:border-gray-50/[0.2]':-->
-    <!--                    item.type === 'link',-->
-    <!--                }"-->
-    <!--              >-->
-    <!--                <Anchor-->
-    <!--                  v-if="item.type === 'link'"-->
-    <!--                  :to="item.route ? item.route : undefined"-->
-    <!--                  :href="item.href ? item.href : undefined"-->
-    <!--                  class="flex-1 hover:no-underline capitalize"-->
-    <!--                  >{{ item.text }}</Anchor-->
-    <!--                >-->
-    <!--                <Button-->
-    <!--                  v-else-if="item.type === 'button'"-->
-    <!--                  :text="item.text"-->
-    <!--                  size="xs"-->
-    <!--                  class="flex-1 font-extrabold capitalize"-->
-    <!--                  :to="item.route ? item.route : undefined"-->
-    <!--                  :href="item.href ? item.href : undefined"-->
-    <!--                />-->
-    <!--              </li>-->
-    <!--            </ul>-->
-    <!--          </nav>-->
-    <!--          <div class="mt-6 text-sm font-bold capitalize">-->
-    <!--            {{ $t('components.theme_switcher.change_theme') }}-->
-    <!--          </div>-->
-    <!--          <div class="mt-2">-->
-    <!--            <ThemeSwitcher type="select-box" />-->
-    <!--          </div>-->
-    <!--          <div class="mt-6 text-sm font-bold capitalize">-->
-    <!--            {{ $t('components.language_switcher.change_language') }}-->
-    <!--          </div>-->
-    <!--          <div class="mt-2">-->
-    <!--            <LanguageSwitcher type="select-box" />-->
-    <!--          </div>-->
-    <!--        </ActionSheetBody>-->
-    <!--        <Button-->
-    <!--          type="secondary"-->
-    <!--          title="Github"-->
-    <!--          href="https://github.com/viandwi24/nuxt3-awesome-starter"-->
-    <!--        >-->
-    <!--          &lt;!&ndash;          <IconMdi:github-face />&ndash;&gt;-->
-    <!--          <span class="ml-1">Github</span>-->
-    <!--        </Button>-->
-    <!--        <Button-->
-    <!--          text="Close"-->
-    <!--          type="secondary"-->
-    <!--          @click.prevent="toggleOptions(false)"-->
-    <!--        />-->
-    <!--      </ActionSheet>-->
-    <!--    </template>-->
+        <template #options="{ toggleOptions }">
+          <ActionSheet @onClose="toggleOptions(false)">
+            <ActionSheetBody>
+              <ActionSheetHeader text="Menu" />
+              <nav class="leading-6 font-semibold text-gray-600 dark:text-gray-300">
+                <ul class="flex flex-col">
+                  <li
+                    v-for="(item, i) in menus"
+                    :key="i"
+                    class="flex w-full"
+                    :class="{
+                      'pb-2 mb-2 border-b border-gray-900/10 dark:border-gray-50/[0.2]':
+                        item.type === 'link',
+                    }"
+                  >
+                    <Anchor
+                      v-if="item.type === 'link'"
+                      :to="item.route ? item.route : undefined"
+                      :href="item.href ? item.href : undefined"
+                      class="flex-1 hover:no-underline capitalize"
+                      >{{ item.text }}</Anchor
+                    >
+                    <Button
+                      v-else-if="item.type === 'button'"
+                      :text="item.text"
+                      size="xs"
+                      class="flex-1 font-extrabold capitalize"
+                      :to="item.route ? item.route : undefined"
+                      :href="item.href ? item.href : undefined"
+                    />
+                  </li>
+                </ul>
+              </nav>
+<!--              <div class="mt-6 text-sm font-bold capitalize">-->
+<!--                {{ $t('components.theme_switcher.change_theme') }}-->
+<!--              </div>-->
+<!--              <div class="mt-2">-->
+<!--                <ThemeSwitcher type="select-box" />-->
+<!--              </div>-->
+<!--              <div class="mt-6 text-sm font-bold capitalize">-->
+<!--                {{ $t('components.language_switcher.change_language') }}-->
+<!--              </div>-->
+<!--              <div class="mt-2">-->
+<!--                <LanguageSwitcher type="select-box" />-->
+<!--              </div>-->
+            </ActionSheetBody>
+<!--            <Button-->
+<!--              type="secondary"-->
+<!--              title="Github"-->
+<!--              href="https://github.com/viandwi24/nuxt3-awesome-starter"-->
+<!--            >-->
+<!--              &lt;!&ndash;          <IconMdi:github-face />&ndash;&gt;-->
+<!--              <span class="ml-1">Github</span>-->
+<!--            </Button>-->
+            <Button
+              text="Close"
+              type="secondary"
+              @click.prevent="toggleOptions(false)"
+            />
+          </ActionSheet>
+        </template>
   </BuilderNavbar>
 </template>
